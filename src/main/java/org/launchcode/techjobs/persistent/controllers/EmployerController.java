d9a28876bcdaf4fbd5aa1e728b8e37fcadfa2e02
Carl <cindymadewhat@gmail.com>
2022-11-07 19:07:52 -0600

    Pt 2 complete

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,10 +15,22 @@ import java.util.Optional;
 @RequestMapping("employers")
 public class EmployerController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @GetMapping("")
+    public String index (Model model){
+        model.addAttribute("title", "All Employers");
+        model.addAttribute("employer", employerRepository.findAll());
+        return "employers/index";
+    }
+
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
+        model.addAttribute("title", "Add Employer");
         model.addAttribute(new Employer());
+        /*model.addAttribute("employer", employerRepository.findAll());*/
         return "employers/add";
     }
 
@@ -27,14 +41,14 @@ public class EmployerController {
         if (errors.hasErrors()) {
             return "employers/add";
         }
-
+        employerRepository.save(newEmployer);
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -8,11 +8,16 @@ import javax.validation.constraints.Size;
 public class Employer extends AbstractEntity {
 
     @NotBlank(message = "Location is required")
-    @Size(min = 3, max = 150, message = "location must be between 3 and 150 characters")
+    @Size(min = 3, max = 150, message = "Location must be between 3 and 150 characters")
     private String location;
 
     public String getLocation() {
         return location;
     }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
     public Employer() {}
 }
